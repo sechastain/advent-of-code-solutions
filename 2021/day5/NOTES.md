188c541e91c5206082008efbdd26516791c60b07
Spencer <spencer.chastain@firstperformance.com>
2021-12-05 13:03:26 -0500

    Adding input for a different leaderboard for day 4; nodejs impl for day5

---
+++ 2021/day5/NOTES.md
@@ -0,0 +1,8 @@
+
+Started out trying to do kotlin, but since I'm running Catalina, I cannot install xcode 12.5 for
+kotlin-native
+
+So, switched over to the day job language - node js
+
+Pretty happy with this initial solution - not really any re-work for the second part
+
---
+++ 2021/day5/day5.a.js
@@ -0,0 +1,76 @@
+
+const fs = require('fs');
+
+let lines = fs.readFileSync(process.argv[2]).toString().split('\n');
+
+function buildSegment(line) {
+  console.log('line: ', line);
+  let [pt1, _, pt2] = line.split(' ');
+  pt1 = pt1.split(',');
+  pt2 = pt2.split(',');
+  return {
+    x1: parseInt(pt1[0]),
+    y1: parseInt(pt1[1]),
+    x2: parseInt(pt2[0]),
+    y2: parseInt(pt2[1])
+  };
+}
+
+function isHorVer(segment) {
+  return segment.x1 === segment.x2 || segment.y1 === segment.y2;
+}
+
+function plotHorizontal(x1, y1, x2, y2) {
+  let sign = x2 > x1 ? 1 : -1
+  return [...Array(Math.abs(x2-x1) + 1).keys()].map(off => {
+    return {
+      x: x1 + sign * off,
+      y: y1
+    };
+  });
+}
+
+function plotVertical(x1, y1, x2, y2) {
+  let sign = y2 > y1 ? 1 : -1
+  return [...Array(Math.abs(y2-y1) + 1).keys()].map(off => {
+    return {
+      x: x1,
+      y: y1 + sign * off
+    }
+  });
+}
+
+function plotSegment(segment) {
+  let {x1, y1, x2, y2} = segment;
+  if(x1 === x2) {
+    return plotVertical(x1, y1, x2, y2);
+  } else if(y1 === y2) {
+    return plotHorizontal(x1, y1, x2, y2);
+  }
+}
+
+function applyPlot(plotmap, plotpoints) {
+  plotpoints.forEach(pt => {
+    let xcol = plotmap[pt.x] = plotmap[pt.x] || {};
+    xcol[pt.y] = (xcol[pt.y] || 0) + 1
+  });
+  return plotmap;
+}
+
+const segments = lines.filter(x => !!x).map(buildSegment).filter(isHorVer);
+const plotted = segments.map(plotSegment).reduce(applyPlot, {});
+
+console.log(plotted);
+
+let intersections = 0;
+Object.keys(plotted).forEach(xcol => {
+  let col = plotted[xcol];
+  Object.keys(col).forEach(y => {
+    intersections += (col[y] > 1 ? 1 : 0);
+  });
+});
+
+console.log(intersections);
+
+
+
