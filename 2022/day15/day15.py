4bc4ef0d5098c1fe7ccbe0933611b13a8f40cb42
Spencer Chastain <spencer@getpequity.com>
2022-12-15 13:47:12 -0500

    2022 day 15 part 1 refactor, part 2

--- 2022/day15/day15.py
+++ 2022/day15/day15.py
@@ -6,34 +6,81 @@ import json
 from ..utils.io import readlines
 from functools import reduce, cmp_to_key
 
+def smoosh(alist, xrange):
+  if len(alist) == 0:
+    return [xrange]
+  
+  lxrange = alist[-1]
+  if lxrange[1] >= xrange[0]:
+    if lxrange[1] < xrange[1]:
+      lxrange[1] = xrange[1]
+  else:
+    alist.append(xrange)
+
+  return alist
+
+def clip(bound):
+  def _clip(alist, x):
+    if x[0] < 0 and x[1] < 0:
+      return alist
+    elif x[0] > bound and x[1] > bound:
+      return alist
+    if x[0] < 0:
+      x[0] = 0
+    if x[1] > bound:
+      x[1] = bound
+    alist.append(x)
+    return alist
+
+  return _clip
+    
 class Board:
   def __init__(self):
     self.occupied = set()
     self.objs = dict()
 
-  def _get_covered_points(self, pt, y):
+  def _get_xrange(self, pt, y):
     ydist = abs(y - pt['sensor'][1])
     maxdist = pt['dist']
     xunits = (maxdist - ydist)
     x0 = pt['sensor'][0]
-    return [(x0 + x, y) for x in range(-xunits, xunits+1)]
+    return [x0 + x for x in [-xunits, xunits]]
 
-  def not_beacons(self, y):
+  def _get_coverage(self, y):
     coverage = [p for p in self.objs.values() if y >= p['min_y'] and y <= p['max_y']]
-    covered = set()
-    for pt in coverage:
-      covered.update(self._get_covered_points(pt, y))
-    covered = [c for c in covered if c not in self.occupied]
-    return len(covered)
+    covered = [self._get_xrange(pt, y) for pt in coverage]
+    covered.sort(key=lambda x: x[0])
+    covered = reduce(smoosh, covered, [])
+    return covered
 
-  def uncovered_points(self, bound):
-    pass
+  def _count_occupied(self, xranges, y):
+    occ = [o for o in self.occupied if o[1] == y]
+    hit = 0
+    for o in occ:
+      for xr in xranges:
+        if o[0] >= xr[0] and o[0] <= xr[1]:
+          hit += 1
+    return hit
+      
+  def not_beacons(self, y):
+    covered = self._get_coverage(y)
+    clen = sum([x[1] - x[0] + 1 for x in covered])
+    occ = self._count_occupied(covered, y)
+    return clen - occ
+
+  def find_uncovered_points(self, bound):
+    for y in range(bound+1):
+      covered = self._get_coverage(y)
+      if len(covered) > 1:
+        covered = reduce(clip(bound), covered, [])
+        if len(covered) > 1:
+          return covered[0][1] * bound + y
+    return -1
     
   def add_coverage(self, p1, p2):
     self.occupied.add(p1)
     self.occupied.add(p2)
     dist = abs(p2[0] - p1[0]) + abs(p2[1] - p1[1])
-    print('dist', dist)
     self.objs[p1] = {
       'sensor': p1,
       'beacon': p2,
@@ -54,3 +101,7 @@ board = readlines(sys.argv[1], parseLine, Board())
 
 print(board.not_beacons(10))
 print(board.not_beacons(2000000))
+
+print(board.find_uncovered_points(20))
+print(board.find_uncovered_points(4000000))
+
